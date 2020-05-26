04280a12d86bddcda158f439558109f24ec4f1fb
irene <mora.coro.12@gmail.com>
2020-05-26 02:10:15 -0500

    practica8

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<p>HOLA MUNDO</p>
-	<h1> irene</h1>
-</body>
-</html>
+<html>  
+  <head>
+    <script src="https://aframe.io/releases/0.8.0/aframe.min.js"></script> 
+    <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
+  </head>   
+
+  <body>
+    <a-scene embedded arjs>
+      <a-entity scale=".2 .2 .2"> 
+        <a-entity gltf-model="este.glb" scale="3 3 3" crossOrigin="anonymous">
+        </a-entity>
+        <a-marker-camera preset='hiro'></a-marker-camera>
+        </a-scene>
+      </body>
+    </html>
